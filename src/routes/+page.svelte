ca6e5e3fba6ca625f5bde9bd9acc3f9e9b21bc8f
Gerald Haagsma <haagsmag@gmail.com>
2024-05-21 18:20:47 -0700

    Add basic page content and wide screen styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,41 @@
-<h1>Your Fearless Leader</h1>
+<script lang="ts">
+	import '../styles/fonts.css';
+	import bandPhotograph from '../images/tiles.png';
+</script>
+
+<main>
+	<h1>Your Fearless Leader</h1>
+	<img alt="Photograph of Your Fearless Leader band members" src={bandPhotograph} />
+	<p>
+		We’re a San Francisco based indie rock band known for our dynamic sound and intricate orchestral
+		arrangements. Blending powerful vocals with contemplative lyrics, we create music that moves
+		between explosive energy and haunting stillness. We’re continuing to captivate audiences
+		nationwide with our precise arrangements, energizing performances, and unforgettable sound.
+	</p>
+</main>
 
 <style>
+	:global(body) {
+		background-color: rgb(212, 113, 113);
+		margin: 0;
+	}
+
+	main {
+		color: #222222;
+		font-family: 'NotoSansMono';
+		max-width: 600px;
+		margin: 48px auto;
+		text-align: center;
+	}
+
 	h1 {
+		color: rgb(237, 229, 214);
+		font-family: 'YesevaOne';
+		font-size: 56px;
+	}
+
+	img {
+		height: 600px;
+		width: 600px;
 	}
 </style>
