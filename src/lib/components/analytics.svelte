ea7f88fb5205971224fbb87a3aad6daa2845c1b9
Gerald Haagsma <haagsmag@gmail.com>
2024-05-24 07:33:40 -0700

    Add analytics

---
+++ src/lib/components/analytics.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { dev } from '$app/environment';
+  const GTAG_MEASUREMENT_ID = 'G-H74S4LHGS3';
+</script>
+
+<svelte:head>
+  {#if !dev}
+    <script
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=${GTAG_MEASUREMENT_ID}`}
+    ></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', GTAG_MEASUREMENT_ID);
+    </script>
+  {/if}
+</svelte:head>
