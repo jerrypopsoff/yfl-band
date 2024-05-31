352b4b09c87a6bd039eb055c8fac632ddb24492b
Jerry Haagsma <haagsmag@gmail.com>
2024-05-31 14:43:47 -0700

    Update bio

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -18,12 +18,8 @@ export const ANNOUNCEMENT_URL = '';
 /**
  * Biographical text for the band.
  */
-export const BIO_TEXT = `We’re a San Francisco based indie rock band known for
-our dynamic sound and intricate orchestral arrangements. Blending powerful
-vocals with contemplative lyrics, we create music that moves between explosive
-energy and haunting stillness. We’re continuing to captivate audiences
-nationwide with our precise arrangements, energizing performances, and
-unforgettable sound.`;
+export const BIO_TEXT = `San Francisco based indie rock band known for precise
+  arrangements, energizing performances, and unforgettable sound.`;
 
 /**
  * Spotify embedded player URL. When replacing, be sure to remove everything in
